4dcdbaa7bfbf7260b1891e597da4f8fa0b5c7b91
Charly Escalona <charly2221@hotmail.fr>
2022-07-10 19:46:17 +0200

    Création de la page login

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    SectionList,
+    SafeAreaView,
+    Image,
+    FlatList,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native';
+
+
+const LoginScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>PAGE LOGIN</Text>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
+
+
+export default LoginScreen;
