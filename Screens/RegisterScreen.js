135f1b47d6d3be40711e94c9a8f592393b937336
Charly Escalona <charly2221@hotmail.fr>
2022-07-10 19:46:26 +0200

    Création de la page register

---
+++ Screens/RegisterScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    SectionList,
+    SafeAreaView,
+    Image,
+    FlatList,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native';
+
+
+
+
+const RegisterScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>PAGE REGISTER</Text>
+        </View>
+
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
+
+
+export default RegisterScreen;
