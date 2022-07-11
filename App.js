79b131978a2106db511912f00a91825d3ced302c
Charly Escalona <charly2221@hotmail.fr>
2022-07-11 21:17:29 +0200

    Ajout du header sur l'écran de connexion / inscription

--- App.js
+++ App.js
@@ -5,19 +5,89 @@ import Navigation from './Navigation'
 import LoginScreen from './Screens/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen'
 import ProfileScreen from './Screens/ProfileScreen'
+import HomeScreen from './Screens/HomeScreen'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
+
+const logged = false;
+
 export default function App() {
 
   return (
       <NavigationContainer>
           <Stack.Navigator>
               <Stack.Screen name="Tabs" component={Navigation} options={{ headerShown: false }} />
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Register" component={RegisterScreen} />
-              <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+              <Stack.Screen name="Login" options={{
+                                title: 'Larguez les amarres',
+                                headerStyle: {
+                                    backgroundColor: '#48B781',
+                                },
+                                headerTintColor: '#fff',
+                                headerTitleStyle: {
+                                    fontWeight: 'bold',
+                                    fontFamily:"Syne_700Bold",
+                                    color:"#FFF",
+                                    fontSize:20
+                                },
+                                headerBackVisible:false
+                            }}
+                            component={LoginScreen} />
+              <Stack.Screen name="Register"
+                            options={{
+                                title: 'Larguez les amarres',
+                                headerStyle: {
+                                    backgroundColor: '#48B781',
+                                },
+                                headerTintColor: '#fff',
+                                headerTitleStyle: {
+                                    fontWeight: 'bold',
+                                    fontFamily:"Syne_700Bold",
+                                    color:"#FFF",
+                                    fontSize:20
+                                },
+                                headerBackVisible:false
+                            }}r
+                            component={RegisterScreen} />
+
+
+              {logged === false &&
+                  <Stack.Screen
+                      name="Profile"
+                      component={HomeScreen}
+                      options={{
+                          title: 'Larguez les amarres',
+                          headerStyle: {
+                              backgroundColor: '#48B781',
+                          },
+                          headerTintColor: '#fff',
+                          headerTitleStyle: {
+                              fontWeight: 'bold',
+                              fontFamily:"Syne_700Bold",
+                              color:"#FFF",
+                              fontSize:20
+                          },
+                          headerBackVisible:false
+                      }}
+                  />
+              }
+              {logged === true &&
+                  <Stack.Screen option={{
+                      headerStyle: {
+                          backgroundColor: '#f4511e',
+                      },
+                      headerTintColor: '#fff',
+                      headerTitleStyle: {
+                          fontWeight: 'bold',
+                      },
+                      headerTitleAlign: 'left',
+                      headerTitle:"Larguez les amarres",
+                  }}
+                      name="Profile"
+                      component={ProfileScreen} />
+              }
+
           </Stack.Navigator>
       </NavigationContainer>
 
