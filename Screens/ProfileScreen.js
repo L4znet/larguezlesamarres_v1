14429e02662e72b78512286d0ebd1a56cf53db38
Charly Escalona <charly2221@hotmail.fr>
2022-07-12 15:21:18 +0200

    Ajout d'une scrollview pour masquer la dangerzone

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,29 +1,32 @@
-import {Text, View, Button, TouchableOpacity, StyleSheet, Image} from "react-native";
+import {Text, View, Button, TouchableOpacity, StyleSheet, Image, ScrollView} from "react-native";
 import { Entypo } from '@expo/vector-icons'
 
 
 const ProfileScreen = ({ navigation }) => {
     return (
-        <View style={styles.container}>
+        <ScrollView>
             <View style={styles.container}>
-                <Image
-                    style={styles.profilPicture}
-                    source={require('../assets/default.png')}
-                />
-                <Text style={styles.username}>John Doe</Text>
-                <View  style={styles.loginCredentials}>
-                    <Text style={styles.loginCredentials.title}> Vos informations de connexion</Text>
-                    <Text style={styles.loginCredentials.email}>doejohn@helloworld.com</Text>
-                </View>
+                <View style={styles.container}>
+                    <Image
+                        style={styles.profilPicture}
+                        source={require('../assets/default.png')}
+                    />
+                    <Text style={styles.username}>John Doe</Text>
+                    <View  style={styles.loginCredentials}>
+                        <Text style={styles.loginCredentials.title}> Vos informations de connexion</Text>
+                        <Text style={styles.loginCredentials.email}>doejohn@helloworld.com</Text>
+                    </View>
 
-                <TouchableOpacity style={styles.editProfil}><Text style={styles.editProfil.editProfilText}>Modifier mon profil</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.logout}><Text style={styles.logout.logoutText}>Déconnexion</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.editProfil}><Text style={styles.editProfil.editProfilText}>Modifier mon profil</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.logout}><Text style={styles.logout.logoutText}>Déconnexion</Text></TouchableOpacity>
+                </View>
+                <View style={styles.dangerZone}>
+                    <Text style={styles.deleteAccount.title}>DANGER ZONE</Text>
+                    <TouchableOpacity style={styles.deleteAccount}><Text style={styles.deleteAccount.deleteAccountText}>Supprimer mon compte</Text></TouchableOpacity>
+                    <Text style={styles.deleteAccount.warningMessage}>Cette action est irréversible</Text>
+                </View>
             </View>
-            <View style={styles.dangerZone}>
-                <Text style={styles.deleteAccount.warningMessage}>Cette action est irréversible</Text>
-                <TouchableOpacity style={styles.deleteAccount}><Text style={styles.deleteAccount.deleteAccountText}>Supprimer mon compte</Text></TouchableOpacity>
-            </View>
-        </View>
+        </ScrollView>
     );
 }
 
@@ -38,11 +41,13 @@ const styles = StyleSheet.create({
     },
     profilPicture:{
         width:150,
-        height:150
+        height:150,
+        marginTop:50
     },
     username:{
         fontSize:30,
-        marginBottom:30
+        marginVertical:20,
+        fontWeight:"bold"
     },
     loginCredentials:{
         width:"100%",
@@ -103,7 +108,8 @@ const styles = StyleSheet.create({
         justifyContent:"center",
         alignItems:"center",
         width:"100%",
-        marginBottom:30
+        height:250,
+        marginVertical:30,
     },
     deleteAccount:{
         width:"80%",
@@ -118,10 +124,17 @@ const styles = StyleSheet.create({
             color:"#FFF",
             fontSize:18
         },
+        title:{
+            color:"#c74a4a",
+            fontSize:30,
+            fontWeight:"bold",
+            marginVertical:40
+        },
         warningMessage:{
             color:"#c74a4a",
-            fontSize:25,
-            fontWeight:"bold"
+            fontSize:20,
+            fontWeight:"bold",
+            marginTop:10
         }
     },
 })
