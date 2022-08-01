dc43a059b4c89f4e6d25bfb8e34b981f4b11b0b9
Charly Escalona <charly2221@hotmail.fr>
2022-08-01 03:39:12 +0200

    Affichages des favoris sur l'écran

--- Screens/FavoriteScreen.js
+++ Screens/FavoriteScreen.js
@@ -1,16 +1,56 @@
-import {Text, View, Button, TouchableHighlight, StyleSheet} from "react-native";
+import {Text, View, Button, TouchableHighlight, StyleSheet, FlatList, TouchableOpacity, Image} from "react-native";
+import {useState} from "react";
+import axios from "axios";
+import {auth} from "../firebase";
+import CardOffer from "../components/CardOffer";
+import {toggleFavoriteAdded} from "../store/statesLoadSlice";
+import {useDispatch, useSelector} from "react-redux";
 
 const FavoriteScreen = ({ navigation }) => {
+
+    const [favorites, setFavorites] = useState("");
+    const dispatch = useDispatch()
+    const favoriteAdded = useSelector((state) => state.statesLoad.favoriteAdded)
+
+    if(favoriteAdded){
+        axios.get('http://192.168.1.24:3000/api/favorite/' + auth.currentUser.uid +'/liked').then((response) => {
+            setFavorites(response.data)
+        })
+        dispatch(toggleFavoriteAdded())
+    }
+
+    const RecentlyItem = ({ item }) => {
+        return (
+           <>
+               <CardOffer item={item} navigation={navigation}/>
+               <TouchableOpacity onPress={() => {}}>
+                   <Text>Supprimer de mes favoris</Text>
+               </TouchableOpacity>
+           </>
+        );
+    };
+
+
     return (
         <View>
-            <Text>fsdsfdsfd</Text>
+            <FlatList
+                style={styles.recentlyItemContainer}
+                data={favorites}
+                renderItem={({ item }) => <RecentlyItem item={item} />}
+                keyExtractor={item => item.key}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+            />
         </View>
     );
 }
 
 
 const styles = StyleSheet.create({
-
+    recentlyItemContainer:{
+        marginLeft:15,
+        marginTop:20
+    },
 })
 
 export default FavoriteScreen;
